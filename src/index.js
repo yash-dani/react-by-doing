89ddf8db127eeeba71eb6d2677a7b8569a4337e0
yash-dani <daniyash19@gmail.com>
2020-06-13 21:15:14 -0400

    generated rendered files successfully

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  <div>
+    <h1>Hello, world! Fortnite is the best</h1>
+    <p> Hit it if you know you lit </p>
+  </div>,
+  document.getElementById('root')
+ );
+/*
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+);*/
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
